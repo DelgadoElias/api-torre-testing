46e052b8157d5548eaa77f1647035812febfd7e0
Elias Lautaro Delgado <82496172+DelgadoElias@users.noreply.github.com>
2022-01-14 06:47:24 -0300

    Testing
    Trying to fix it up

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const { conn } = require('./src/db.js');
 require('dotenv').config()
 
 // Syncing all the models at once.
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 5000, () => {
+  server.listen(server_port, server_host, () => {
     console.log('%s listening at' + process.env.PORT)
   });
 });
