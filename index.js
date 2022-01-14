aafd69d9dfaf0eba47c7360d82e9d5bf47d2593c
Elias Lautaro Delgado <82496172+DelgadoElias@users.noreply.github.com>
2022-01-14 06:52:55 -0300

    Update
    Completing issues?

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const { conn } = require('./src/db.js');
 require('dotenv').config()
 
 // Syncing all the models at once.
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
+server.set('port', process.env.PORT || 8080);
+
 conn.sync({ force: true }).then(() => {
-  server.listen(server_port, server_host, () => {
+  server.listen(server.get('port'), server_host, () => {
     console.log('%s listening at' + process.env.PORT)
   });
 });
