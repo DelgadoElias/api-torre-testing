e0da4edcc806cd935103a62f6f4fa1b6cb64808d
Elias Lautaro Delgado <82496172+DelgadoElias@users.noreply.github.com>
2022-01-14 06:17:10 -0300

    Update
    Fixing deploy

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config()
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(4000, () => {
+  server.listen(process.env.PORT || 5000, () => {
     console.log('%s listening at 3002')
   });
 });
